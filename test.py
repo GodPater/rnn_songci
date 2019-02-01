f5d0881e7edf9f3a6f8f01282b5cbb3a35f91da1
GodPater <809289913@qq.com>
2019-02-01 19:54:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import os
 from tensorflow.python import pywrap_tensorflow
 
-current_path = os.getcwd()
-model_dir = os.path.join(current_path, 'model')
-checkpoint_path = os.path.join(model_dir,'embedding.ckpt-0') # 保存的ckpt文件名，不一定是这个
+# current_path = os.getcwd()
+# model_dir = os.path.join(current_path, 'model')
+# checkpoint_path = os.path.join(model_dir,'embedding.ckpt-0') # 保存的ckpt文件名，不一定是这个
+
 # Read data from checkpoint file
-reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
+reader = pywrap_tensorflow.NewCheckpointReader("/output/model.ckpt-3710")
 var_to_shape_map = reader.get_variable_to_shape_map()
 # Print tensor name and values
 for key in var_to_shape_map:
